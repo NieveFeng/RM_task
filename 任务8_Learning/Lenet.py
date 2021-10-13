be3ce1e86769f56deeb90fdb294e459252cdda62
NieveFeng <1294051350@qq.com>
2021-10-13 22:03:58 +0800

    提交说明

--- 任务8_Learning/Lenet.py
+++ 任务8_Learning/Lenet.py
@@ -2,19 +2,22 @@
 import gzip
 import struct
 import numpy as np
-
 import torch
 from torch import nn
 from torch.nn import functional as F
 from torch.autograd import Variable
-
 from torch.utils.data import TensorDataset, DataLoader
 from torchvision import transforms
 import math
 
+# 定义一些超参数
+use_gpu = torch.cuda.is_available()
+batch_size = 256
+kwargs = {'num_workers': 2, 'pin_memory': True}  # DataLoader的参数
+
 # 读取数据的函数,先读取标签，再读取图片
 def _read(image, label):
-    minist_dir = 'data/'
+    minist_dir = '/home/nieve/feng/RM_task/任务8_Learning/data/'
     with gzip.open(minist_dir + label) as flbl:
         magic, num = struct.unpack(">II", flbl.read(8))
         label = np.fromstring(flbl.read(), dtype=np.int8)
@@ -22,11 +25,9 @@ def _read(image, label):
         magic, num, rows, cols = struct.unpack(">IIII", fimg.read(16))
         image = np.fromstring(fimg.read(), 
         dtype=np.uint8).reshape(len(label), rows, cols)
-    return [image, label]
+    return image, label
 
 # 读取数据
-
-
 def get_data():
     [train_img, train_label] = _read(
         'train-images-idx3-ubyte.gz',
@@ -35,39 +36,80 @@ def get_data():
     [test_img, test_label] = _read(
         't10k-images-idx3-ubyte.gz',
         't10k-labels-idx1-ubyte.gz')
-    return [train_img, train_label, test_img, test_label]
+    return train_img, train_label, test_img, test_label
 
+# 获取数据
+X, y, Xt, yt = get_data()
+
+train_x, train_y = torch.from_numpy(
+    X.reshape(-1, 1, 28, 28)).float(), torch.from_numpy(y.astype(int))
+test_x, test_y = [torch.from_numpy(
+    Xt.reshape(-1, 1, 28, 28)).float(), torch.from_numpy(yt.astype(int))]
+
+# 封装好数据和标签
+train_dataset = TensorDataset(data_tensor=train_x, target_tensor=train_y)
+test_dataset = TensorDataset(data_tensor=test_x, target_tensor=test_y)
+
+# 定义数据加载器
+train_loader = DataLoader(dataset=train_dataset,
+                          shuffle=True, batch_size=batch_size, **kwargs)
+test_loader = DataLoader(dataset=test_dataset,
+                         shuffle=True, batch_size=batch_size, **kwargs)
 
 # 定义lenet5
 class LeNet5(nn.Module):
     def __init__(self):
         '''构造函数，定义网络的结构'''
         super().__init__()
-        # 定义卷积层，1个输入通道，6个输出通道，5*5的卷积filter
-        # 外层补上了两圈0,因为输入的是32*32
-        self.conv1 = nn.Conv2d(1, 6, 5, padding=2)
-        # 第二个卷积层，6个输入，16个输出，5*5的卷积filter
-        self.conv2 = nn.Conv2d(6, 16, 5)
+		
+        #第一层卷积和最大池化
+        #输入:(6*28*28);输出：(6*14*14)
+        self.conv1 = nn.Sequential(     
+            #新建卷积层,kernel_size表示卷积核大小,stride表示步长,padding=2，
+            #图片大小变为 28+2*2 = 32 (两边各加2列0)，保证输入输出尺寸相同
+            nn.Conv2d(1,6,kernel_size=5,stride=1,padding=2), 
+            nn.ReLU(),
+            #可以选择取最大值池化MaxPool2d,也可以选择取平均值池化AvgPool2d,两者参数相同
+            nn.MaxPool2d(kernel_size = 2 ,stride = 2,padding=0)   
+        )
 
-        # 最后是三个全连接层
-        self.fc1 = nn.Linear(16*5*5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        #第二层卷积和最大池化
+        #输入:(6*14*14);输出：(16*5*5)
+        self.conv2 = nn.Sequential(
+            #input_size=(6*14*14)，output_size=(16*10*10)
+            nn.Conv2d(6,16,kernel_size=5,stride=1,padding=0), 
+            nn.ReLU(),
+            #input_size=(16*10*10)，output_size=(16*5*5)
+            nn.MaxPool2d(kernel_size = 2,stride = 2,padding=0)    
+        )
 
-    def forward(self, x):
-        '''前向传播函数'''
-        # 先卷积，然后调用relu激活函数，再最大值池化操作
-        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
-        # 第二次卷积+池化操作
-        x = F.max_pool2d(F.relu(self.conv2(x)), (2, 2))
-        # 重新塑形,将多维数据重新塑造为二维数据，256*400
-        x = x.view(-1, self.num_flat_features(x))
-        print('size', x.size())
-        # 第一个全连接
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
+        #全连接层
+        self.fc1 = nn.Sequential(
+            nn.Linear(16*5*5,120),
+            nn.ReLU()
+        )
+        #全连接层
+        self.fc2 = nn.Sequential(
+            nn.Linear(120,84),
+            nn.ReLU()
+        )
+        #全连接层
+        self.fc3 = nn.Linear(84,10)
+
+
+    #网络前向传播过程
+    def forward(self,x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+
+        #全连接层均使用的nn.Linear()线性结构，输入输出维度均为一维，故需要把数据拉为一维
+        x = x.view(x.size(0), -1) 
+        
+        x = self.fc1(x)
+        x = self.fc2(x)
         x = self.fc3(x)
         return x
+
 
     def num_flat_features(self, x):
         # x.size()返回值为(256, 16, 5, 5)，size的值为(16, 5, 5)，256是batch_size
@@ -77,15 +119,7 @@ class LeNet5(nn.Module):
             num_features *= s
         return num_features
 
-
-# 定义一些超参数
-use_gpu = torch.cuda.is_available()
-batch_size = 256
-kwargs = {'num_workers': 2, 'pin_memory': True}  # DataLoader的参数
-
 # 参数值初始化
-
-
 def weight_init(m):
     if isinstance(m, nn.Conv2d):
         n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
@@ -95,8 +129,6 @@ def weight_init(m):
         m.bias.data.zero_()
 
 # 训练函数
-
-
 def train(epoch):
     # 调用前向传播
     model.train()
@@ -117,8 +149,6 @@ def train(epoch):
                 100. * batch_idx / len(train_loader), loss.data[0]))
 
 # 定义测试函数
-
-
 def test():
     # 让模型变为测试模式，主要是保证dropout和BN和训练过程一致。BN是指batch normalization
     model.eval()  
@@ -138,25 +168,6 @@ def test():
     print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
-
-
-# 获取数据，
-[X, y, Xt, yt] = get_data()
-
-train_x, train_y = torch.from_numpy(
-    X.reshape(-1, 1, 28, 28)).float(), torch.from_numpy(y.astype(int))
-test_x, test_y = [torch.from_numpy(
-    Xt.reshape(-1, 1, 28, 28)).float(), torch.from_numpy(yt.astype(int))]
-
-# 封装好数据和标签
-train_dataset = TensorDataset(data_tensor=train_x, target_tensor=train_y)
-test_dataset = TensorDataset(data_tensor=test_x, target_tensor=test_y)
-
-# 定义数据加载器
-train_loader = DataLoader(dataset=train_dataset,
-                          shuffle=True, batch_size=batch_size, **kwargs)
-test_loader = DataLoader(dataset=test_dataset,
-                         shuffle=True, batch_size=batch_size, **kwargs)
 
 # 实例化网络
 model = LeNet5()
